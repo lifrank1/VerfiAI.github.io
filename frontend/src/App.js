52b2e676844265a92ec8bbff93eafcc77f22eb29
ShashankRaghuraj <80557791+ShashankRaghuraj@users.noreply.github.com>
2025-02-12 17:43:55 -0500

    Yaml file created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from "./pages/HomeScreen";
 import LoginScreen from "./pages/LoginScreen";
 import CreateAccountScreen from "./pages/CreateAccountScreen";
 import Chat from "./pages/chat";
+import ApiDocs from './components/ApiDocs';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/api" element={<ApiDocs />} />
         <Route path="/" element={<HomeScreen />} />
         <Route path="/LoginScreen" element={<LoginScreen />} />
         <Route path="/create-account" element={<CreateAccountScreen />} />
